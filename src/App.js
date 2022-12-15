36007ec4fdae9b1f69a05fa5e3d25bd09252f646
Vijji04 <bn.vijayendradev@gmail.com>
2022-12-15 06:55:33 +0530

    fixed view

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MainCard from "./MainCard.js"
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-between mx-auto space-y-4 p-2">
+    <div className="flex flex-col items-center justify-between mx-auto space-y-4 p-1">
       <Navbar />
       <Rectangle />
       <MainCard />
