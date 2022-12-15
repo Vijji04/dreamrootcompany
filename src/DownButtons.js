fe6fe4f4f02dc8ae1099ca02859157e603f03635
Vijji04 <bn.vijayendradev@gmail.com>
2022-12-15 07:00:09 +0530

    fixed spacing

--- src/DownButtons.js
+++ src/DownButtons.js
@@ -3,11 +3,11 @@ import eye from './eye.svg'
 
 function DownButtons() {
   return (
-    <div className='container mx-5 px-5'>
-        <div className='flex space-x-6'>
+    <div className='container mx-5 px-6'>
+        <div className='flex space-x-5'>
         <button className='text-bold text-xs p-1 px-2 pt-1 text-white bg-gradient-to-r from-[#B1008A] to-[#060D67] w-24 h-8 rounded-full baseline shadow-6xl'>RESCHEDULE</button>
         <button className='text-bold text-xs p-1 px-2 pt-1 text-white bg-gradient-to-r from-[#B1008A] to-[#060D67] w-24 h-8 rounded-full baseline shadow-6xl'>CANCEL</button>
-        <button className='text-bold text-xs p-1 px-2 pt-2 pl-1 flex justify-center outline outline-offset-2 outline-1 border-[#999999]    text-black w-24 h-8 rounded-full baseline shadow-6xl'>
+        <button className='text-bold text-xs p-1 px-2 pt-2 pl-1 flex justify-center outline outline-offset-1 outline-1 border-[#999999]    text-black w-24 h-8 rounded-full baseline shadow-6xl'>
             
             <img className="mx-1"src={eye} alt="" ></img>
             VIEW
--- src/TopButtons.js
+++ src/TopButtons.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function TopButtons() {
   return (
-    <div className='container mx-5 px-5'>
-        <div className='flex space-x-2'>
+    <div className='container mx-5 px-6'>
+        <div className='flex space-x-1'>
         <button className='font-Quicksand text-xs p-2 px-2 pt-2 text-white bg-gradient-to-br from-[#FF8412] to-[#B1008A] w-20 h-8 rounded-full baseline shadow-6xl'>Clinic</button>
         <button className='font-Quicksand text-xs p-2 px-2 pt-2 text-black bg-[#EDEDED] w-20 h-8 rounded-full baseline drop-shadow-lg'>Test</button>
         <button className='font-Quicksand text-xs p-2 px-2 pt-2 text-black bg-[#EDEDED] w-20 h-8 rounded-full baseline drop-shadow-lg'>Surgery</button>
